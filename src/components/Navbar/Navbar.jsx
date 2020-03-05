dcf6cdad7937b3762b5c1b647151a1a7dd80aad3
arthur-ryoo <jene0523@gmail.com>
2020-03-04 22:54:58 -0800

    add conditionalUI

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,23 +2,37 @@ import React from 'react';
 import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
 
+import userService from '../../utils/userService';
+
 const Navbar = props => {
+  const conditionalUI = userService.getUser() ? (
+    <>
+      <li>
+        <Link to="/brands">Coffee Brands</Link>
+      </li>
+      <li>
+        <Link to="" onClick={props.handleLogout}>
+          Logout
+        </Link>
+      </li>
+    </>
+  ) : (
+    <>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/signup">Signup</Link>
+      </li>
+    </>
+  );
+
   return (
     <nav className={styles.navbar}>
       <Link to="/">
         <h1>Rate Coffee</h1>
       </Link>
-      <ul>
-        <li>
-          <Link to="/coffeebrands">Coffee Brands</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-      </ul>
+      <ul>{conditionalUI}</ul>
     </nav>
   );
 };
