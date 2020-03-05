28c28affbeb90093e2d8b3fd5f92b8fe50bb52d9
arthur-ryoo <jene0523@gmail.com>
2020-03-04 21:02:29 -0800

    add back-end bolierplate

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+db.once('connected', () => {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const serveFavicon = require('serve-favicon');
+const logger = require('morgan');
+
+const app = express();
+
+require('dotenv').config();
+require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(serveFavicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/coffeebrands', require('./routes/api/coffeebrands'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+require('dotenv').config();
+require('./config/database');
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`);
+});
