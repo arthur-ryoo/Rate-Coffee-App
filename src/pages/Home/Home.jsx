107cfdc5e04831b391cf4d3b80b6f0131975fb1c
arthur-ryoo <jene0523@gmail.com>
2020-03-07 10:59:42 -0800

    add link to component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './Home.module.css';
 
 const Home = props => {
   return (
     <main>
-      <h1>Recently Added Brands</h1>
+      <div className={styles.header}>
+        <h1>Recently Added Brands</h1>
+      </div>
       {props.recentlyAddedBrands.map(({ name, description, _id }) => (
-        <section key={_id}>
-          <h1>{name}</h1>
-          <p>{description}</p>
-        </section>
+        <div className={styles.div}>
+          <Link to={`/details/${_id}`}>
+            <section key={_id}>
+              <h1>{name}</h1>
+              <p>{description}</p>
+            </section>
+          </Link>
+        </div>
       ))}
     </main>
   );
