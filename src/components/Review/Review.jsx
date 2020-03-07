88dfc96b93338e17055e3f9e9b382060d613ae85
arthur-ryoo <jene0523@gmail.com>
2020-03-07 15:02:06 -0800

    change display logic

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -10,35 +10,14 @@ const Review = props => {
         <h1>Ratings</h1>
       </div>
       <div className={styles.div}>
-        {props.reviews.map(({ _id }) => (
-          <section key={_id}></section>
+        {props.reviews.map(({ taste, price, comment, addedBy, brandId }) => (
+          <section key={brandId}>
+            <h1>{addedBy.name}</h1>
+            <p>Taste - {taste}</p>
+            <p>Price - {price}</p>
+            <small>{comment}</small>
+          </section>
         ))}
-        <h1>
-          {
-            props.reviews.filter(r => r.brandId === props.match.params.id)[0]
-              .addedBy.name
-          }
-        </h1>
-        <p>
-          Taste -
-          {
-            props.reviews.filter(r => r.brandId === props.match.params.id)[0]
-              .taste
-          }
-        </p>
-        <p>
-          Price -
-          {
-            props.reviews.filter(r => r.brandId === props.match.params.id)[0]
-              .price
-          }
-        </p>
-        <small>
-          {
-            props.reviews.filter(r => r.brandId === props.match.params.id)[0]
-              .comment
-          }
-        </small>
       </div>
     </main>
   );
