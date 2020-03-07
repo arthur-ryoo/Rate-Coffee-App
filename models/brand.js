d74eaa8dd3b8d50f1337b0c93dca4e8dfb81241f
arthur-ryoo <jene0523@gmail.com>
2020-03-07 10:59:18 -0800

    update description type

--- models/brand.js
+++ models/brand.js
@@ -7,7 +7,10 @@ const brandSchema = new Schema(
       type: String,
       required: true
     },
-    description: String
+    description: {
+      type: String,
+      required: true
+    }
   },
   { timestamps: true }
 );
