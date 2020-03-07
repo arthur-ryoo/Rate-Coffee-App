c2d94db5d81fe11852b907759d28cee3f4b2c6a5
arthur-ryoo <jene0523@gmail.com>
2020-03-07 10:54:41 -0800

    add year info

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 const Footer = props => {
   return (
     <footer className={styles.footer}>
-      <p>Copyright &copy; Rate Coffee</p>
+      <p>Copyright &copy; Rate Coffee {new Date().getFullYear()}</p>
     </footer>
   );
 };
